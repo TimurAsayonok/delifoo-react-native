1221a351c896b71d787840a1e21d72c3ed2ccb5e
Timur Asayonok <timurasayonok@MacBook-Pro-Timur.local>
2017-04-11 13:02:45 +0200

    Some components have been imported into code

--- DeliFoo/index.ios.js
+++ DeliFoo/index.ios.js
@@ -5,6 +5,10 @@
  */
 
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReduxers, compose } from 'redux';
+import { thunkMiddleware } from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import {
   AppRegistry,
   StyleSheet,
