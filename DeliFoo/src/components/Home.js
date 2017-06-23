ea1a9b98e9f645e23efb0de196784541e53746ac
Timur Asayonok <t.asaynok@gmail.com>
2017-06-23 17:37:21 +0200

    Added styles and textInput on the Home page

--- DeliFoo/src/components/Home.js
+++ DeliFoo/src/components/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {
+  Image,
   View,
   Text,
+  TextInput,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  StyleSheet
 } from 'react-native'
 
 class Home extends React.Component {
@@ -12,29 +15,84 @@ class Home extends React.Component {
     super(props)
 
     this.state={
-
+      ingredientsInput: '',
+      isSearching: false
     }
   }
 
   searchPressed(){
-    this.props.fetchRecipes('bacon,cucumber,banana');
+    this.setState({isSearching: true});
+    this.props.fetchRecipes(this.state.ingredientsInput).then( () => {
+      this.setState({isSearching: false});
+    });
+  }
+
+  getRecipes(){
+    return Object.keys(this.props.recipes).map(key => this.props.recipes[key])
   }
 
   render(){
     console.log(this.props);
+
     return (
-      <View>
-        <View style={{marginTop: 20}}>
-          <TouchableOpacity onPress={() => this.searchPressed()}>
+      <View style={styles.scene}>
+        <View style={styles.searchSection}>
+          <TextInput style={styles.searchSectionTextInput}
+            returnKeyType='search'
+            placeholder='Type ingredients here (comma delimited)'
+            onChangeText={(ingredientsInput) => this.setState({ingredientsInput})}
+            value={this.state.ingredientsInput}
+          />
+          <TouchableOpacity style={styles.searchSectionButton} onPress={() => this.searchPressed()}>
           <Text>Search Recipes</Text>
           </TouchableOpacity>
         </View>
-        <ScrollView>
+        <ScrollView style={styles.scrollSection}>
+          {!this.state.isSearching && this.getRecipes().map((recipe) => {
+            let i = 0;
+            return <View style={styles.recipeSection}>
+              <Image source={{uri: recipe.thumbnail}} style={styles.image} />
+              <Text style={styles.title}>{recipe.title}</Text>
+              <Text style={styles.ingredients}>{recipe.ingredients}</Text>
+            </View>
+          })}
+          {this.state.isSearching ? <Text>Searching ...</Text> : null}
         </ScrollView>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    marginTop: 20
+  },
+  searchSection: {
+    height: 30,
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    padding: 5,
+    flexDirection: 'row'
+  },
+  searchSectionTextInput: {
+    flex: 0.7
+  },
+  searchSectionButton: {
+    flex: 0.3
+  },
+  scrollSection: {
+    flex: 0.8,
+  },
+  recipeSection: {
+    marginBottom: 10,
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+  image: {
+    height: 150
+  }
+});
 
 function mapStateToProps(state) {
   return{
